7f54d814e21f695faa21fec8faea15f9b6f36f06
tanaymittal25 <tanay.mittal25@gmail.com>
2019-07-05 22:50:41 +0530

    New

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+app = express();
+
+app.listen(8080, (err) => {
+    if(err)
+        console.log(`Error`);
+    console.log(`Running on Port 8080`);
+});
