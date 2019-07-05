991e03bab92a3962ee366d206f011304454ac237
tanaymittal25 <tanay.mittal25@gmail.com>
2019-07-05 23:21:16 +0530

    New

---
+++ config/secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT : process.env.PORT || 8080,
+    Database : process.env.Database || 'mongodb://TanayProfile:Tanay2598@ds247637.mlab.com:47637/profileinfo'
+}
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const config = require('./config/secret');
 
 app = express();
 
-app.listen(8080, (err) => {
+app.listen(config.PORT, (err) => {
     if(err)
         console.log(`Error`);
-    console.log(`Running on Port 8080`);
+    console.log(`Running on Port ${config.PORT}`);
 });
