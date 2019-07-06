91f66051ebe8c2198b503d20f0bfdca9eb1dda77
tanaymittal25 <tanay.mittal25@gmail.com>
2019-07-06 09:40:08 +0530

    Mongo

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const config = require('./config/secret');
 
 app = express();
+
+mongoose.connect(config.Database, {useNewUrlParser: true}, (err) => {
+    if(err)
+        console.log(`Error with DB`);
+    console.log(`Connected to Database`);
+})
 
 app.listen(config.PORT, (err) => {
     if(err)
